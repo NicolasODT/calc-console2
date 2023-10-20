cf5881d9a544c59de70223e20949fa1692d6b8eb
nicolasODT <nicolas.terwagne@gmail.com>
2023-10-20 19:38:56 +0200

    Add while to check if INT

--- CalcConsole.java
+++ CalcConsole.java
@@ -8,13 +8,19 @@ public class CalcConsole {
 
         while (true) {
             System.out.println("Choisissez une opération: \n" +
-            "[1] Addition\n" +
-            "[2] Soustraction\n" +
-            "[3] Multiplication\n" +
-            "[4] Division\n" +
-            "[5] Quitter\n" +
-            "Entrez votre choix : ");
+                    "[1] Addition\n" +
+                    "[2] Soustraction\n" +
+                    "[3] Multiplication\n" +
+                    "[4] Division\n" +
+                    "[5] Quitter\n" +
+                    "Entrez votre choix : ");
 
+            // Vérification si l'entrée est un nombre entier
+            while (!scan.hasNextInt()) {
+                System.out.println("Ce n'est pas un nombre. Veuillez entrer un nombre.");
+                scan.next();
+            }
+            // Récupération du choix
             int choix = scan.nextInt();
 
             if (choix == 5) {
@@ -28,24 +34,25 @@ public class CalcConsole {
             System.out.print("Entrez le deuxième nombre : ");
             int num2 = scan.nextInt();
 
-                switch (choix) {
-                    case 1:
-                        System.out.println("Résultat de l'addition: " + Addition.calculer(num1, num2));
-                        break;
-                    case 2:
-                        System.out.println("Résultat de la soustraction: " + Soustraction.calculer(num1, num2));
-                        break;
-                    case 3:
-                        System.out.println("Résultat de la multiplication: " + Multiplication.calculer(num1, num2));
-                        break;
-                    case 4:
-                        System.out.println("Résultat de la division: " + Division.calculer(num1, num2));
-                        break;
-                    default:
-                        System.out.println("Choix invalide. Veuillez réessayer.");
-                        break;
-                }
+            switch (choix) {
+                case 1:
+                    System.out.println("Résultat de l'addition: " + Addition.calculer(num1, num2));
+                    break;
+                case 2:
+                    System.out.println("Résultat de la soustraction: " + Soustraction.calculer(num1, num2));
+                    break;
+                case 3:
+                    System.out.println("Résultat de la multiplication: " + Multiplication.calculer(num1, num2));
+                    break;
+                case 4:
+                    System.out.println("Résultat de la division: " + Division.calculer(num1, num2));
+                    break;
+                default:
+                    System.out.println("Choix invalide. Veuillez réessayer.");
+                    break;
+            }
         }
+        // Fermeture du scanner pour éviter les fuites de mémoire
         scan.close();
     }
 }
